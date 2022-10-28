5e8cdb20e085c79603839eb07d199b6a53881a5d
jeezass_dk <xjeezassx@gmail.com>
2022-10-28 21:12:29 +0700

    completed

--- task19_hw/Program.cs
+++ task19_hw/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Ввведите число:");
+int N = int.Parse(Console.ReadLine());
+if (N >= 10000 && N < 100000)
+{
+    Palindrome(N);
+}
+else
+{
+    Console.WriteLine("Вы ввели не верное число, число должно быть пятизначным");
+}
+
+void Palindrome(int number)
+{
+    if (((number / 10000) == (number % 10)) && ((number / 1000 % 10) == (number % 100 / 10)))
+    {
+        Console.WriteLine($"Число {number} - является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine($"Число {number} - не является палиндромом");
+    }
+}
